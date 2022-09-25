4f5ef2262f374018f0c5e87faeaa893b49a52be8
Kristina <ajiaha@gmail.com>
2022-09-25 19:48:27 +0300

    1.0

--- app/src/main/java/ru/krisproject/calkulator/MainActivity.java
+++ app/src/main/java/ru/krisproject/calkulator/MainActivity.java
@@ -2,17 +2,38 @@ package ru.krisproject.calkulator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText pass;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+        pass = (EditText)findViewById(R.id.editText);
+        btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btn.setText("Done");
+                btn.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+                Toast.makeText(MainActivity.this,pass.getText(),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     public void onButtonClick(View v) {
