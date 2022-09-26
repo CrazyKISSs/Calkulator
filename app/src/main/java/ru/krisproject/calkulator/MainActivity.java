e27cfbb09237b3ef2e7d81daf7b7f42eea17cf82
Kristina <ajiaha@gmail.com>
2022-09-26 09:40:26 +0300

    1.0

--- app/src/main/java/ru/krisproject/calkulator/MainActivity.java
+++ app/src/main/java/ru/krisproject/calkulator/MainActivity.java
@@ -1,20 +1,25 @@
 package ru.krisproject.calkulator;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText pass;
-    private Button btn, btn2;
+    private Button btn, btn2, btn_alert;
+    private RatingBar rating;
+    private TextView text_show;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         pass = (EditText)findViewById(R.id.editText);
         btn = (Button)findViewById(R.id.button);
         btn2 = (Button)findViewById(R.id.other_btn);
+        btn_alert = (Button)findViewById(R.id.allert2);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,12 +49,45 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "Ups....",Toast.LENGTH_LONG).show();
             }
         });
+
+        rating = (RatingBar)findViewById(R.id.ratingBar);
+        text_show = (TextView)findViewById(R.id.textView);
+        rating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                text_show.setText("Значение: " + String.valueOf(rating));
+            }
+        });
+
+        btn_alert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder a_builder = new AlertDialog.Builder(MainActivity.this);
+                a_builder.setMessage("Вы хотите закрыть приложение?")
+                        .setCancelable(false)
+                        .setPositiveButton("Да", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                finish();
+                            }
+                        })
+                        .setNegativeButton("Нет", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = a_builder.create();
+                alert.setTitle("Закрытие приложения");
+                alert.show();
+            }
+        });
     }
 
     public void onButtonClick(View v) {
         EditText el1 = (EditText)findViewById(R.id.Num1);
         EditText el2 = (EditText)findViewById(R.id.Num2);
-        TextView resText = (TextView)findViewById(R.id.Result);
+        TextView resText = (TextView)findViewById(R.id.alert);
 
         int num1 = Integer.parseInt(el1.getText().toString());
         int num2 = Integer.parseInt(el2.getText().toString());
